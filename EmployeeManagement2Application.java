a5a4866d930ec1e6cc0171f3d9f8035bd6562e99
SaketPoonam <120573545+SaketPoonam@users.noreply.github.com>
2023-05-30 13:28:04 +0530

    Add files via upload

---
+++ EmployeeManagement2Application.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagement2Application.class, args);
+	}
+
+}
---
+++ Person1Application.java
@@ -0,0 +1,13 @@
+package com.person1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Person1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Person1Application.class, args);
+	}
+
+}
